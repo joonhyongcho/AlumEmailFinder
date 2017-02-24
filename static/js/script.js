592f8436591cd0641addaab78fd48f15217f75ee
joonhyongcho <joonhyongcho@gmail.com>
2017-02-23 20:41:14 -0500

    first commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,27 @@
 (function() {
 
 }).call(this);
+
+$("#send_name").on('click', function() {
+
+  $.ajax({
+  url: "https://api-lookup.dartmouth.edu/v1/lookup",
+  type: "get", //send it through get method
+  data: {
+    q: $("#sendName").val(),
+    includeAlum: true,
+  },
+  success: function(response) {
+    for (var i = 0; i < response.users.length; i++) {
+      var person = response.users[i];
+      console.log(person);
+      $(".people").append(person.displayName + '\t' + person.mail + '<br>');
+    }
+    console.log(response)
+  },
+  error: function(xhr) {
+    //Do Something to handle error
+  }
+});
+    // console.log($("#sendName").val());
+});
